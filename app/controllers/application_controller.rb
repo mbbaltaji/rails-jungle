a3609c95476d3f65b2e98fa3b2bc91ab14804e0f
mbbaltaji <baltaji@uwindsor.ca>
2022-07-12 16:39:43 -0400

    add HTTP basic Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   
   protect_from_forgery with: :exception
-
+  http_basic_authenticate_with :name => "jungle", :password => "book" 
   private
 
   def cart
