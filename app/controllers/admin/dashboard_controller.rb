56a4df8cff1df447a79da52eea9530023948bd08
mbbaltaji <baltaji@uwindsor.ca>
2022-07-12 16:51:25 -0400

    feat

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
+
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: "jungle", password: "book"
+  
   def show
+    @product_count = Product.count
+  
+    @category_count = Product.group(:category_id).count
   end
 end
