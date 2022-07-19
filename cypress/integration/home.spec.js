3674023db5a7c7aa22f6014a3c9d9e1bcf0e856a
mbbaltaji <baltaji@uwindsor.ca>
2022-07-19 17:48:20 -0400

    add cypress tests for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe('home page testing', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
