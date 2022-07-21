6d2c13ff583440ef549bda3550b7d4033400e866
mbbaltaji <baltaji@uwindsor.ca>
2022-07-21 16:13:09 -0400

    add logic to log users in and out

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+
+  def new 
+  end
+  
+  def create #for receiving the submitted form and creating a user with the form's parameters
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
+  end
+end  
+  
