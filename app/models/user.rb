41b5a4967f687280d8524d916634053bcbec4d00
mbbaltaji <baltaji@uwindsor.ca>
2022-07-21 16:36:13 -0400

    add logic to complete user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+
+
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: { case_sensitive: true }
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :password, length: { minimum: 4 }
+end
